bd8bea924fd809a834518d78843b34a954cf2a8a
Mayank Rawat <mayankrawat@Mayanks-MacBook-Air.local>
2026-09-20 03:37:35 +0530

    Fixed api call

--- client/src/pages/onboarding/CreatePortfolio.jsx
+++ client/src/pages/onboarding/CreatePortfolio.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { apiRequest } from "../../services/api";
+import { apiRequest } from "../../utils/api";
 import { auth } from "../../firebase/firebase";
 
 import {
